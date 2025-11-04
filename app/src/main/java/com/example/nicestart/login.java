384206767b4514f84c7f3de3eac9eead2ebe77ad
Álvaro Bueno <alvarobueno@MacBook-Air-de-Alvaro.local>
2025-11-04 13:16:40 +0100

    Botones cambiados

--- app/src/main/java/com/example/nicestart/login.java
+++ app/src/main/java/com/example/nicestart/login.java
@@ -1,6 +1,8 @@
 package com.example.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,10 @@ public class login extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void openMain(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
