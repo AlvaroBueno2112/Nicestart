161681136ef5ff5d2774581487d4500c8423195f
alvarobueno <thebestplayer1216@gmail.com>
2025-11-13 13:57:08 +0100

    Creado el perfil y añadidas imagenes mediante glide.

--- app/src/main/java/com/example/nicestart/Profile.java
+++ app/src/main/java/com/example/nicestart/Profile.java
@@ -1,8 +1,6 @@
 package com.example.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,23 +11,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 
-public class signup extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_profile);
 
-        ImageView mSea = findViewById(R.id.backViewSign);
+        ImageView mSea = findViewById(R.id.imagenProfile);
 
         Glide.with(this)
-                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .load("https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29uYXxlbnwwfHwwfHx8MA%3D%3D")
                 //.transition(DrawableTransitionOptions.withCrossFade(100))
-                .centerCrop()
+                //.centerCrop()
                 //.diskCacheStrategy(DiskCacheStrategy.ALL)
                 //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.white)))
-//                .circleCrop()
+                .circleCrop()
                 .into(mSea);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -38,13 +36,4 @@ public class signup extends AppCompatActivity {
             return insets;
         });
     }
-
-    public void openMain(View v){
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-
 }
--- app/src/main/java/com/example/nicestart/login.java
+++ app/src/main/java/com/example/nicestart/login.java
@@ -3,12 +3,15 @@ package com.example.nicestart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class login extends AppCompatActivity {
 
@@ -17,6 +20,18 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        ImageView mSea = findViewById(R.id.backViewLogin);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                //.transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                //.diskCacheStrategy(DiskCacheStrategy.ALL)
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.white)))
+//                .circleCrop()
+                .into(mSea);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
